879fcf1b56824a1c71824b76ad9611ec305899b5
Steve Choi <stevech071@gmail.com>
2022-05-19 10:55:32 -0700

    dev info tool box roughdraft

--- react-app/src/components/DevInfo/index.js
+++ react-app/src/components/DevInfo/index.js
@@ -9,8 +9,18 @@ const DevInfo = () => {
                 <div className="dev-icon-button"></div>
                 <div className="dev-info-box-content">
                 <h1 className='dev-info-title'>Developer Info</h1>
+                <div className='info-pill-box'>
                     <p className='label-tag'>Name</p>
-                    <p className='label-tag-value'>Steve Choi</p>
+                    <p className='label-tag-text'>Steve Choi</p>
+                </div>
+                <div className='info-pill-box'>
+                    <p className='label-tag'>Contacts</p>
+                    <p className='label-tag-text'>1,2,3</p>
+                </div>
+                <div className='info-pill-box'>
+                    <p className='label-tag'>Other Works</p>
+                    <p className='label-tag-text'>1,2,3</p>
+                </div>
                 </div>
             </div>
         </>
